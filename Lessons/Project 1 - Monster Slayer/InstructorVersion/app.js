c3ad089515a198e6ba7a8ea047142aa9e62c79b0
craigmiller160 <craigmiller160@gmail.com>
2018-03-31 10:35:42 -0400

    Finished the instructor's project

--- Lessons/Project 1 - Monster Slayer/InstructorVersion/app.js
+++ Lessons/Project 1 - Monster Slayer/InstructorVersion/app.js
@@ -4,23 +4,35 @@
         data: {
             playerHealth: 100,
             monsterHealth: 100,
-            gameIsRunning: false
+            gameIsRunning: false,
+            turns: []
         },
         methods: {
             startGame() {
                 this.gameIsRunning = true;
                 this.playerHealth = 100;
                 this.monsterHealth = 100;
+                this.turns = [];
             },
             attack() {
-                this.monsterHealth -= this.calculateDamage(3, 10);
+                const damage = this.calculateDamage(3, 10);
+                this.monsterHealth -= damage;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player hits Monster for ' + damage
+                });
                 if (this.checkWin()) {
                     return;
                 }
                 this.monsterAttacks();
             },
             specialAttack() {
-                this.monsterHealth -= this.calculateDamage(10, 20);
+                const damage = this.calculateDamage(10, 20);
+                this.monsterHealth -= damage;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player hits Monster hard for ' + damage
+                });
                 if (this.checkWin()) {
                     return;
                 }
@@ -33,15 +45,23 @@
                 else {
                     this.playerHealth = 100;
                 }
-
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player heals for 10'
+                });
                 this.monsterAttacks();
             },
             giveUp() {
                 this.gameIsRunning = false;
             },
             monsterAttacks() {
-                this.playerHealth -= this.calculateDamage(5, 12);
+                const damage = this.calculateDamage(5, 12);
+                this.playerHealth -= damage;
                 this.checkWin();
+                this.turns.unshift({
+                    isPlayer: false,
+                    text: 'Monster hits Player for ' + damage
+                });
             },
             calculateDamage(min, max) {
                 return Math.max(Math.floor(Math.random() * max) + 1, min);
