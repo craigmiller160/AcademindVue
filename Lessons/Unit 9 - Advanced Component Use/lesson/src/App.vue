04e152f3bf47368c61208f80b8a365791aadcd47
craigmiller160 <craigmiller160@gmail.com>
2018-04-01 17:51:01 -0400

    More work with slots

--- Lessons/Unit 9 - Advanced Component Use/lesson/src/App.vue
+++ Lessons/Unit 9 - Advanced Component Use/lesson/src/App.vue
@@ -2,15 +2,31 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-
+        <app-quote>
+          <template slot="title">
+            <h2>{{ quoteTitle }}</h2>
+          </template>
+          <template>
+            <p>A wonderful Quote!</p>
+          </template>
+        </app-quote>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
+  import Quote from './components/Quote';
 
+  export default {
+    components: {
+      'app-quote': Quote
+    },
+    data() {
+      return {
+        quoteTitle: 'The Quote'
+      }
+    }
   }
 </script>
 
