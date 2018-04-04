ca97f3ab96b581705379b5429550bb51c58c9ced
Craig Miller <craigmiller160@gmail.com>
2018-04-03 21:01:22 -0400

    Binding a basic form input

--- Lessons/Unit 11 - Handling User Input With Forms/forms/src/App.vue
+++ Lessons/Unit 11 - Handling User Input With Forms/forms/src/App.vue
@@ -7,24 +7,15 @@
                     <hr>
                     <div class="form-group">
                         <label for="email">Mail</label>
-                        <input
-                            type="text"
-                            id="email"
-                            class="form-control">
+                        <input type="text" id="email" class="form-control" v-model="email" />
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            class="form-control">
+                        <input type="password" id="password" class="form-control" />
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
-                        <input
-                            type="number"
-                            id="age"
-                            class="form-control">
+                        <input type="number" id="age" class="form-control" />
                     </div>
 
                 </div>
@@ -33,26 +24,17 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="message">Message</label><br>
                     <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                        id="message"
-                        rows="5"
-                        class="form-control"></textarea>
+                    <textarea id="message" rows="5" class="form-control"></textarea>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <div class="form-group">
                         <label for="sendmail">
-                            <input
-                                type="checkbox"
-                                id="sendmail"
-                                value="SendMail"> Send Mail
+                            <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
                         </label>
                         <label for="sendInfomail">
-                            <input
-                                type="checkbox"
-                                id="sendInfomail"
-                                value="SendInfoMail"> Send Infomail
+                            <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
                         </label>
                     </div>
 
@@ -61,25 +43,17 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="male">
-                        <input
-                            type="radio"
-                            id="male"
-                            value="Male"> Male
+                        <input type="radio" id="male" value="Male" /> Male
                     </label>
                     <label for="female">
-                        <input
-                            type="radio"
-                            id="female"
-                            value="Female"> Female
+                        <input type="radio" id="female" value="Female" /> Female
                     </label>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
                     <label for="priority">Priority</label>
-                    <select
-                        id="priority"
-                        class="form-control">
+                    <select id="priority" class="form-control">
                         <option></option>
                     </select>
                 </div>
@@ -87,9 +61,7 @@
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button
-                        class="btn btn-primary">Submit!
-                    </button>
+                    <button class="btn btn-primary">Submit!</button>
                 </div>
             </div>
         </form>
@@ -101,7 +73,7 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
+                        <p>Mail: {{ email }}</p>
                         <p>Password:</p>
                         <p>Age:</p>
                         <p>Message: </p>
@@ -121,7 +93,11 @@
 
 <script>
     export default {
-
+        data() {
+            return {
+                email: ''
+            }
+        }
     }
 </script>
 
