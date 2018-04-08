b71212eeff6bda8f23ad09a7acc4d6f28f47b55a
Craig Miller <craigmiller160@gmail.com>
2018-04-08 18:31:15 -0400

    Learned all form inputs

--- Lessons/Unit 11 - Handling User Input With Forms/forms/src/App.vue
+++ Lessons/Unit 11 - Handling User Input With Forms/forms/src/App.vue
@@ -7,15 +7,15 @@
                     <hr>
                     <div class="form-group">
                         <label for="email">Mail</label>
-                        <input type="text" id="email" class="form-control" v-model="email" />
+                        <input type="text" id="email" class="form-control" v-model="userData.email" />
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" id="password" class="form-control" />
+                        <input type="password" id="password" class="form-control" v-model.lazy="userData.password" />
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
-                        <input type="number" id="age" class="form-control" />
+                        <input type="number" id="age" class="form-control" v-model.number="userData.age" />
                     </div>
 
                 </div>
@@ -24,37 +24,44 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="message">Message</label><br>
                     <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea id="message" rows="5" class="form-control"></textarea>
+                    <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <div class="form-group">
-                        <label for="sendmail">
-                            <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
-                        </label>
+                        <div class="checkbox-inline">
+                            <label for="sendmail">
+                                <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail" /> Send Mail
+                            </label>
+                        </div>
+                        <div class="checkbox-inline">
+                            <label for="sendInfomail">
+                                <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail" /> Send Infomail
+                            </label>
+                        </div>
                     </div>
-
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input type="radio" id="male" value="Male" /> Male
-                    </label>
-                    <label for="female">
-                        <input type="radio" id="female" value="Female" /> Female
-                    </label>
+                    <div class="radio-inline">
+                        <label for="male">
+                            <input type="radio" id="male" value="Male" v-model="gender" /> Male
+                        </label>
+                    </div>
+                    <div class="radio-inline">
+                        <label for="female">
+                            <input type="radio" id="female" value="Female" v-model="gender" /> Female
+                        </label>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
                     <label for="priority">Priority</label>
-                    <select id="priority" class="form-control">
-                        <option></option>
+                    <select id="priority" class="form-control" v-model="selectedPriority">
+                        <option v-for="priority in priorities">{{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -73,16 +80,16 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail: {{ email }}</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="mail in sendMail">{{ mail }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -92,10 +99,25 @@
 </template>
 
 <script>
+    const priorityOpts = [
+        'High',
+        'Medium',
+        'Low'
+    ];
+
     export default {
         data() {
             return {
-                email: ''
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 27
+                },
+                message: 'A new Text',
+                sendMail: [],
+                gender: 'Male',
+                priorities: priorityOpts,
+                selectedPriority: priorityOpts[1]
             }
         }
     }
