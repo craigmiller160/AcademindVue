1a92f4efa28fde7ba4ef23f6746f9f56359f27a8
Craig Miller <craigmiller160@gmail.com>
2018-04-11 12:11:06 -0400

    Added more routing features

--- Lessons/Unit 14 - Routing/routing/src/routes.js
+++ Lessons/Unit 14 - Routing/routing/src/routes.js
@@ -5,10 +5,26 @@ import UserDetail from './components/user/UserDetail';
 import UserEdit from './components/user/UserEdit';
 
 export const routes = [
-    {path: '', component: Home},
-    {path: '/user', component: User, children: [
-        {path: '', component: UserStart},
-        {path: ':id', component: UserDetail},
-        {path: ':id/edit', component: UserEdit}
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: '/user',
+        component: User,
+        children: [
+            {
+                path: '',
+                component: UserStart
+            },
+            {
+                path: ':id',
+                component: UserDetail
+            },
+            {
+                path: ':id/edit',
+                component: UserEdit,
+                name: 'userEdit'
+            }
     ]}
 ];
