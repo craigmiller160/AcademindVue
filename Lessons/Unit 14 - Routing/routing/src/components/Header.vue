66de5e6f68680223ac6b61b71dcbe236a378f39f
Craig Miller <craigmiller160@gmail.com>
2018-04-11 07:14:39 -0400

    Added route params

--- Lessons/Unit 14 - Routing/routing/src/components/Header.vue
+++ Lessons/Unit 14 - Routing/routing/src/components/Header.vue
@@ -3,8 +3,11 @@
         <router-link tag="li" to="/" active-class="active" exact>
             <a>Home</a>
         </router-link>
-        <router-link tag="li" to="/user" active-class="active">
-            <a>User</a>
+        <router-link tag="li" to="/user/1" active-class="active">
+            <a>User 1</a>
+        </router-link>
+        <router-link tag="li" to="/user/2" active-class="active">
+            <a>User 2</a>
         </router-link>
     </ul>
 </template>
--- Lessons/Unit 14 - Routing/routing/src/components/user/User.vue
+++ Lessons/Unit 14 - Routing/routing/src/components/user/User.vue
@@ -2,15 +2,27 @@
     <div>
         <h1>The User Page</h1>
         <hr />
+        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params['id']
+            }
+        },
         methods: {
             navigateToHome() {
                 this.$router.push({path: '/'});
+            }
+        },
+        watch: {
+            '$route'(to, from) {
+                this.id = to.params['id'];
+                //This is better than computed because more behavior can be executed here
             }
         }
     }
