964f421fd7c964a937d838bf7a1bb2c50cd41415
Craig Miller <craigmiller160@gmail.com>
2018-04-11 07:23:05 -0400

    Added some nested routes

--- Lessons/Unit 14 - Routing/routing/src/routes.js
+++ Lessons/Unit 14 - Routing/routing/src/routes.js
@@ -1,13 +1,14 @@
 import User from './components/user/User';
 import Home from './components/Home';
+import UserStart from './components/user/UserStart';
+import UserDetail from './components/user/UserDetail';
+import UserEdit from './components/user/UserEdit';
 
 export const routes = [
-    {
-        path: '',
-        component: Home
-    },
-    {
-        path: '/user/:id',
-        component: User
-    }
+    {path: '', component: Home},
+    {path: '/user', component: User, children: [
+        {path: '', component: UserStart},
+        {path: ':id', component: UserDetail},
+        {path: ':id/edit', component: UserEdit}
+    ]}
 ];
