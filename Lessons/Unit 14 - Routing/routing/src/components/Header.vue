9dacd5cd19613782f745fd91cd70a176aff7fe08
Craig Miller <craigmiller160@gmail.com>
2018-04-10 23:00:00 -0400

    Added controls for routing

---
+++ Lessons/Unit 14 - Routing/routing/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link tag="li" to="/" active-class="active" exact>
+            <a>Home</a>
+        </router-link>
+        <router-link tag="li" to="/user" active-class="active">
+            <a>User</a>
+        </router-link>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'header'
+    }
+</script>
+
+<style scoped>
+
+</style>
