8f4736dbc91951e8cfb3bc7358ac03f032821fc9
Craig Miller <craigmiller160@gmail.com>
2018-04-10 22:49:23 -0400

    Added router

---
+++ Lessons/Unit 14 - Routing/routing/src/routes.js
@@ -0,0 +1,13 @@
+import User from './components/user/User';
+import Home from './components/Home';
+
+export const routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: '/user',
+        component: User
+    }
+];
