def2ebbb7d8871a100bebf833be598ff48058c64
Craig Miller <craigmiller160@gmail.com>
2018-04-15 17:33:09 -0400

    Built more stock logic

--- Lessons/Unit 18 - Stock Trader Project/stock-trader/src/components/stocks/Stock.vue
+++ Lessons/Unit 18 - Stock Trader Project/stock-trader/src/components/stocks/Stock.vue
@@ -1,10 +1,51 @@
 <template>
-    <h1>The stocks stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <!--<label for="quantity">Quantity</label>--> <!-- TODO use Bootstrap 4 and align this stuff -->
+                    <input id="quantity" type="number" class="form-control" placeholder="quantity" v-model="quantity" />
+                </div>
+                <div class="pull-right">
+                    <button type="button" class="btn btn-success" @click="buyStock" :disabled="disableBuyBtn">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'stock'
+        props: [
+            'stock'
+        ],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        },
+        computed: {
+            disableBuyBtn() {
+                return this.quantity <= 0 || isNaN(this.quantity);
+            }
+        }
     }
 </script>
 
--- Lessons/Unit 18 - Stock Trader Project/stock-trader/src/components/stocks/Stocks.vue
+++ Lessons/Unit 18 - Stock Trader Project/stock-trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,42 @@
 <template>
-    <h1>The stocks component</h1>
+    <div class="row">
+        <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+    </div>
 </template>
 
 <script>
+    import Stock from './Stock';
+
     export default {
-        name: 'stocks'
+        data() {
+            return {
+                stocks: [
+                    {
+                        id: 1,
+                        name: 'BMW',
+                        price: 110
+                    },
+                    {
+                        id: 2,
+                        name: 'Google',
+                        price: 200
+                    },
+                    {
+                        id: 3,
+                        name: 'Apple',
+                        price: 250
+                    },
+                    {
+                        id: 4,
+                        name: 'Twitter',
+                        price: 8
+                    }
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
     }
 </script>
 
