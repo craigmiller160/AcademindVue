da0278dac39fce44559cecdf64b9c25076a8d831
craigmiller160 <craigmiller160@gmail.com>
2018-04-21 12:43:31 -0400

    Tiny tweaks

--- Lessons/Unit 18 - Stock Trader Project/stock-trader/src/store/modules/stocks.keys.js
+++ Lessons/Unit 18 - Stock Trader Project/stock-trader/src/store/modules/stocks.keys.js
@@ -1,6 +1,8 @@
 export const MUTATION_SET_STOCKS = 'stocks/setStocks';
 export const MUTATION_RANDOM_STOCKS = 'stocks/randomStocks';
+
 export const ACTION_BUY_STOCK = 'stocks/buyStock';
 export const ACTION_INIT_STOCKS = 'stocks/initStocks';
 export const ACTION_RANDOMIZE_STOCKS = 'stocks/randomizeStocks';
+
 export const GETTER_STOCKS = 'stocks/stocks';
