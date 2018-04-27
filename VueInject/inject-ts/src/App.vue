87b0efd8808fccd3c696d19874b842ef268afb1a
Craig Miller <craigmiller160@gmail.com>
2018-04-26 21:12:11 -0400

    Think I fixed part of the git issue

---
+++ VueInject/inject-ts/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <p style="font-weight: bold;" :style="messageColor">{{ msg }}</p>
+        <button type="button" @click="changeMessageColor">Change Color</button>
+        <br />
+        <input type="number" v-model="count" />
+        <p>{{ count }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class App extends Vue {
+        msg = 'This is my message';
+        count = 0;
+        isBlue = false;
+
+        get messageColor(): string {
+            return this.isBlue ? 'color: blue;' : 'color: black;';
+        }
+
+        changeMessageColor(): void {
+            this.isBlue = !this.isBlue;
+        }
+    }
+</script>
+
+<style>
+
+</style>
