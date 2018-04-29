2215a9e19dab47b0240e46e5e6396bd657864914
Craig Miller <craigmiller160@gmail.com>
2018-04-29 08:04:07 -0400

    Scaffolded meetup application

--- Vuetify/vuetify-start/src/main.js
+++ Vuetify/vuetify-start/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-// import 'material-design-icons-iconfont/dist/material-design-icons.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify);
@@ -11,3 +10,5 @@ new Vue({
     el: '#app',
     render: h => h(App)
 });
+
+
