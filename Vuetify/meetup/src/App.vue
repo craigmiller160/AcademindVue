bcc5971ad86c892b4d3b7615aff32cd9904d56c3
Craig Miller <craigmiller160@gmail.com>
2018-04-29 08:45:42 -0400

    Build toolbar and sidebar

--- Vuetify/meetup/src/App.vue
+++ Vuetify/meetup/src/App.vue
@@ -1,91 +1,69 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+    <v-app>
+        <v-navigation-drawer temporary overflow absolute v-model="sideNav">
+            <v-list>
+                <v-list-tile v-for="item in menuItems" :key="item.title">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark class="primary"> <!-- The colors come from prebuilt classes -->
+            <v-toolbar-side-icon @click="toggleSideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
+            <v-toolbar-title>DevMeetup</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat v-for="item in menuItems" :key="item.title">
+                    <v-icon left>{{ item.icon }}</v-icon>
+                    {{ item.title }}
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <main>
+
+        </main>
+    </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+    export default {
+        data() {
+            return {
+                sideNav: false,
+                menuItems: [
+                    {
+                        icon: 'supervisor_account',
+                        title: 'View Meetups'
+                    },
+                    {
+                        icon: 'room',
+                        title: 'Organize Meetup'
+                    },
+                    {
+                        icon: 'person',
+                        title: 'Profile'
+                    },
+                    {
+                        icon: 'face',
+                        title: 'Sign Up'
+                    },
+                    {
+                        icon: 'lock_open',
+                        title: 'Sign In'
+                    }
+                ]
+            }
+        },
+        methods: {
+            toggleSideNav() {
+                this.sideNav = !this.sideNav;
+            }
+        }
     }
-  },
-  name: 'App'
-}
 </script>
+
+<style>
+
+</style>
